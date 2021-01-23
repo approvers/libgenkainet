a73ed39d5a477cd57cb8be4038b73b924bc00e2e
Naoki Ikeguchi <root@siketyan.dev>
2021-01-23 23:15:15 +0900

    ✨ Add an example

---
+++ example/index.js
@@ -0,0 +1,52 @@
+const { DefaultHandlerFactory, MessagePacket, Node } = require('libgenkainet');
+const { RTCPeerConnection } = require('wrtc');
+
+const connectionFactory = {
+  create() {
+    return new RTCPeerConnection();
+  },
+};
+
+const bob = new Node(
+  'bob',
+  {},
+  new DefaultHandlerFactory({
+    handle(message) {
+      console.log(`bob received: ${message}`);
+    },
+  }),
+  connectionFactory,
+);
+
+const alice = new Node(
+  'alice',
+  {
+    async discover(offer) {
+      console.log('alice is trying to connect to bob');
+
+      return (await bob.accept(offer))[1];
+    }
+  },
+  new DefaultHandlerFactory({
+    handle(message) {
+      console.log(`alice received: ${message}`);
+    }
+  }),
+  connectionFactory,
+);
+
+(async () => {
+  const connection = await alice.connect({
+    id: 'bob',
+  });
+
+  console.log('connection established');
+
+  connection.send(
+    new MessagePacket(
+      alice,
+      bob,
+      'hello!',
+    ),
+  );
+})();

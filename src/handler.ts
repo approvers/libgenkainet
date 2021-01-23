f8911ff56603335d620c7226f6b28a2ebb9614e9
Naoki Ikeguchi <root@siketyan.dev>
2021-01-23 20:53:24 +0900

    ✨ Handler implementation

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,29 @@
-import { IPacket } from './packet';
+import { IPacket, IPacketHandler } from './packet';
+import { Node } from './node';
 
 export interface IHandler {
-  handle(packet: IPacket): void | Promise<void>;
+  handle(packet: IPacket): Promise<void>;
+}
+
+export class Handler {
+  constructor(
+    private readonly _packetHandlers: IPacketHandler<IPacket>[] = [],
+  ) {
+  }
+
+  async handle(packet: IPacket): Promise<void> {
+    const promise = this._packetHandlers
+      .find(handler => handler.supports(packet))
+      ?.handle(packet)
+    ;
+
+    if (promise !== undefined) {
+      await promise;
+    }
+  }
+
+  static default(node: Node): Handler {
+    return new Handler([
+    ]);
+  }
 }

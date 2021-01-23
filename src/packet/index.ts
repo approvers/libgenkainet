d5023ca85aa0a39dd771e60442fc433c962bb0f4
Naoki Ikeguchi <root@siketyan.dev>
2021-01-23 20:52:13 +0900

    ✨ Add IPacketHandler declaration

--- src/packet/index.ts
+++ src/packet/index.ts
@@ -2,3 +2,8 @@ export interface IPacket {
   type: string;
   ttl: number;
 }
+
+export interface IPacketHandler<T extends IPacket> {
+  handle(packet: T): void | Promise<void>;
+  supports(packet: IPacket): boolean;
+}
